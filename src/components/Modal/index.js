db83d2e1e8c14d5c1eeba54dceee7afbb85b6ecc
Wayne Edwards <waedwards42@yahoo.com>
2021-07-17 21:40:57 -0700

    added modal for PhotoList and Modal components

---
+++ src/components/Modal/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Modal({ onClose, currentPhoto}) {
+    const {name, category, description, index} = currentPhoto;
+
+    return (
+        // JSX 
+        <div className="modalBackdrop">
+  <div className="modalContainer">
+    <h3 className="modalTitle">{name}</h3>
+    <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
+    <p>
+      {description}
+    </p>
+    <button onClick={onClose} type="button">
+      Close this modal
+    </button>
+  </div>
+</div>
+
+
+    );
+}
+
+export default Modal;
